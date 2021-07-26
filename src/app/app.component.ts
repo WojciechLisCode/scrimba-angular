233b5f460a454b7ea0cdfc86ddb9b379887cec25
WojciechLisCode <wlis58372+code@gmail.com>
2021-07-26 16:47:20 +0200

    first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` <h1>Hello World</h1> `,
+  template: ` <app-customers></app-customers> `,
 })
 export class AppComponent implements OnInit {
   constructor() {}
