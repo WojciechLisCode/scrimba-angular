da6cf187c3c3c0da4b123a6a8ac072ad528b9dee
WojciechLisCode <wlis58372+code@gmail.com>
2021-07-26 14:58:41 +0200

    hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <h1>Hello World</h1> `,
 })
-export class AppComponent {
-  title = 'scrimba-angular';
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
 }
